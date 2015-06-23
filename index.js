afd9366f31f0c233d47a0c93fd57681972e71ad1
Ben Evans <ben@bensbit.co.uk>
2015-06-23 13:12:17 +0100

    remove redis publish code

--- index.js
+++ index.js
@@ -5,53 +5,16 @@ module.exports.attach = function (store) {
   var instanceId = store.instanceId;
   
   var subClient = redis.createClient(options.port, options.host, options);
-  var pubClient = redis.createClient(options.port, options.host, options);
   
   store.on('subscribe', subClient.subscribe.bind(subClient));
   store.on('unsubscribe', subClient.unsubscribe.bind(subClient));
-  store.on('publish', function (channel, data) {
-    if (data instanceof Object) {
-      try {
-        data = 'o:' + JSON.stringify(data);
-      } catch (e) {
-        data = 's:' + data;
-      }
-    } else {
-      data = 's:' + data;
-    }
-    
-    if (instanceId != null) {
-      data = instanceId + '/' + data;
-    }
-    
-    pubClient.publish(channel, data);
-  });
+
   
   var instanceIdRegex = /^[^\/]*\//;
   
   subClient.on('message', function (channel, message) {
     var sender = null;
-    message = message.replace(instanceIdRegex, function (match) {
-      sender = match.slice(0, -1);
-      return '';
-    });
-    
-    // Do not publish if this message was published by 
-    // the current SC instance since it has already been
-    // handled internally
-    if (sender == null || sender != instanceId) {
-      var type = message.charAt(0);
-      var data;
-      if (type == 'o') {
-        try {
-          data = JSON.parse(message.slice(2));
-        } catch (e) {
-          data = message.slice(2);
-        }
-      } else {
-        data = message.slice(2);
-      }
-      store.publish(channel, data);
-    }
+    var data = JSON.parse(message)
+    store.publish(channel, data);
   });
 };
